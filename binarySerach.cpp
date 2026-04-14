67e99d3880633a10767e133eeee747d3324a0ba6
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-04-14 10:40:56 +0700

    commit1

---
+++ binarySerach.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n===================================\n";
+    cout << "        Masukkan Elemen Array       \n";
+    cout << "=====================================\n";
+}
